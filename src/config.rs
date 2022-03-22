7bc5463d4a949a8ebf67d55309b2437166197a0a
ElnuDev <elnu@elnu.com>
2022-03-21 19:24:58 -0700

    Write boilerplate

---
+++ src/config.rs
@@ -0,0 +1,2 @@
+pub const WINDOW_WIDTH: u32 = 512;
+pub const WINDOW_HEIGHT: u32 = 512;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use sfml::graphics::*;
+use sfml::system::*;
+use sfml::window::*;
+
+mod config;
+use config::*;
+
 fn main() {
-    println!("Hello, world!");
+    let context_settings = ContextSettings::default();
+    let mut window = RenderWindow::new(
+        VideoMode::new(WINDOW_WIDTH, WINDOW_HEIGHT, 16),
+        "bubbler",
+        Style::TITLEBAR | Style::CLOSE,
+        &context_settings,
+    );
+
+    window.display();
+
+    while window.is_open() {
+        loop {
+            match window.poll_event() {
+                Some(event) => match event {
+                    Event::Closed | Event::KeyPressed {
+                        code: Key::Q,
+                        alt: _,
+                        ctrl: true,
+                        shift: _,
+                        system: _,
+                    } => window.close(),
+                    _ => {}
+                },
+                None => break
+            }
+        }
+    }
 }
